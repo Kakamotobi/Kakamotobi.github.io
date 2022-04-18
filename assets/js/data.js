7f5a569edbfca76292e72de9e74ebd0ba4e32e5a
Daeram Chung <daeram.chung@gmail.com>
2022-04-18 11:47:50 +0900

    Add lang data

---
+++ assets/js/data.js
@@ -0,0 +1,36 @@
+const data = {
+	eng: {
+		bioName: "Daeram Chung",
+		bioGreetings: "Hi there 👋",
+		bioAboutMe:
+			"I am an aspiring front-end developer 1 year into programming. I am immersed in contemplation of writing code that is clean and refine. Above all, I realize the value of dedication and consistency, which I am actualizing through daily commits. My self-learning journey has been a splendid one and I am keen to continue my endeavors.",
+		downloadResumeKor: "Resume (KOR)",
+		downloadResumeEng: "Resume (ENG)",
+		projectOneDesc:
+			"A minimalistic React application for managing our daily todos, providing functionality to add, edit, check-off and remove todos.",
+		projectTwoDesc:
+			"A web application that uses OpenWeatherMap's API to display the weather at your current location and another location.",
+		projectThreeDesc:
+			"A landing page responsive layout challenge provided by frontendmentor.io.",
+		emailFormGuide:
+			"You can contact me on this form or via <a href='mailto:daeram.chung@gmail.com'>daeram.chung@gmail.com</a>.",
+		formSubmitBtn: "Send",
+	},
+	kor: {
+		bioName: "정대람",
+		bioGreetings: "안녕하세요 👋",
+		bioAboutMe:
+			"프로그래밍에 입문한지 1년 된 프론트엔드 개발자 정대람입니다. 프로그래밍의 세계에 매료된 후 깔끔하고 정교한 코드를 구현하는 것에 대해 늘 고민하고 있습니다. 노력과 지속을 통해 목표를 이룰 수 있다는 마음가짐으로, 꾸준히 데일리 커밋을 실천하고 있습니다. 독학을 결심하였던 순간부터 지금까지 프로그래밍은 저에게 매우 흥미롭고 즐거운 여정이었고, 앞으로도 프로그래밍의 무궁무진한 세계가 기대됩니다.",
+		downloadResumeKor: "이력서 (국문)",
+		downloadResumeEng: "이력서 (영문)",
+		projectOneDesc:
+			"리액트를 이용하여 만든 데일리 투두 리스트를 관리할 수 있는 웹 어플리케이션입니다. 추가, 수정, 체크, 삭제 기능을 지원합니다.",
+		projectTwoDesc:
+			"OpenWeatherMap API를 활용하여 현재 위치와 다른 지역의 날씨를 알려주는 웹 어플리케이션입니다.",
+		projectThreeDesc:
+			"랜딩 페이지의 반응적 레이아웃을 구현하는 frontendmentor.io의 챌린지입니다.",
+		emailFormGuide:
+			"연락이 필요하시면 아래 폼에 입력해주시거나 <a href='mailto:daeram.chung@gmail.com'>daeram.chung@gmail.com</a>을 통해 연락해주세요.",
+		formSubmitBtn: "보내기",
+	},
+};
